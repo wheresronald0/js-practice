bbadba97395b13c0fe89c8f827b08a55bc2aed5f
Ron Mason <WHERESRONALD0@GMAIL.COM>
2019-01-05 11:58:20 -0500

    questions 2 completed

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 console.log("howdy partner");
 
-// Problem #1
-//tipleAdd(10)(20)(30) //returns 60
+// // Problem #1
+// //tipleAdd(10)(20)(30) //returns 60
 
-function tripleAdd() {
-  return 10 + 20 + 30;
-}
+// function tripleAdd() {
+//   return 10 + 20 + 30;
+// }
 
-console.log(tripleAdd());
+// console.log(tripleAdd());
 
-//es6
-tripleAdd(() => 10 + 20 + 30);
-console.log(tripleAdd());
+// //es6
+// tripleAdd(() => 10 + 20 + 30);
+// console.log(tripleAdd());
 
-//single function
-function tripleAdd1(num1, num2, num3) {
-  return num1 + num2 + num3;
-}
-console.log(tripleAdd1(10, 20, 30));
+// //single function
+// function tripleAdd1(num1, num2, num3) {
+//   return num1 + num2 + num3;
+// }
+// console.log(tripleAdd1(10, 20, 30));
 
-//couried function
+// //curried function (a function that take in arguments and returns a sequence of functions)
 
-function tripleAdd2(num1) {
-  return function(num2) {
-    return function(num3) {
-      return num1 + num2 + num3;
-    };
-  };
-}
+// function tripleAdd2(num1) {
+//   return function(num2) {
+//     return function(num3) {
+//       return num1 + num2 + num3;
+//     };
+//   };
+// }
 
-console.log(tripleAdd2(10)(20)(30));
+// console.log(tripleAdd2(10)(20)(30));
+
+// Problem #2
+// what is an IIFE and why are they used? Give an example
+// It's an immediately invoked function express that's exectuted right after it's create. this creates/encapsulates a local scope so you're not overwriting global variables by accident
+(function doubleNumber(num) {
+  return num * 2;
+})(5);
